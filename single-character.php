f4bd05b546556135a32ab374b88b7fe3b5254a8c
satorub01-cyber <satorub01@outlook.jp>
2026-04-07 22:23:00 +0900

    ミラクルリーダーとくせい表示への対応

--- single-character.php
+++ single-character.php
@@ -731,6 +731,7 @@ if ($ls_html): ?>
                     foreach ($miracle_lines as $line):
                     ?>
                     <span class="effect-num">（<?php echo $index++; ?>）</span><?php echo $line; ?>
+                    <br>
                     <?php endforeach; ?>
                 <?php endif; ?>
             </div>
